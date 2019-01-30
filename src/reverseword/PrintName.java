d3fe572afc57453bbd89f1b946aedcdc9d1514e9
laffinl <laffinl@sheridancollege.ca>
2019-01-30 08:57:31 -0500

    testing

---
+++ src/reverseword/PrintName.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package reverseword;
+
+/**
+ *
+ * @author laffi
+ */
+public class PrintName {
+    
+    private String _name = "Luc Laffin";
+    
+    public static void main(String[] args) {
+        PrintName name = new PrintName();
+        System.out.println(name._name);
+    }
+    
+}
